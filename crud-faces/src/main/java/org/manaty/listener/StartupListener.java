4d2ee9fedbd39f201b0ec9f1fe9b008f39978956
edward.legaspi <edward.legaspi@manaty.net>
2013-08-04 18:11:42 +0800

    Initial commit.

---
+++ crud-faces/src/main/java/org/manaty/listener/StartupListener.java
@@ -0,0 +1,55 @@
+/*
+ * (C) Copyright 2009-2013 Manaty SARL (http://manaty.net/) and contributors.
+ *
+ * Licensed under the GNU Public Licence, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.manaty.listener;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.inject.Inject;
+
+/**
+ * @author Edward P. Legaspi
+ * @since Jun 7, 2013
+ **/
+@Singleton
+@Startup
+public class StartupListener {
+	@Inject
+	private Logger log;
+
+	@Inject
+	private ResourceBundle resourceBundle;
+
+	@PostConstruct
+	private void init() {
+		log.info("Thank you for running CRUD Faces code. For improvements you may contact the developer at czetsuya@gmail.com");
+
+		Messages.setResolver(new Messages.Resolver() {
+			public String getMessage(String message, Object... params) {
+				if (resourceBundle.containsKey(message)) {
+					message = resourceBundle.getString(message);
+				}
+
+				return MessageFormat.format(message, params);
+			}
+		});
+	}
+
+}
